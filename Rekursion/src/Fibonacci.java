499462889d9c069764367a9056a8070cf5e01b9f
mbrueschweiler <mbrueschweiler@ntb.ch>
2019-11-04 17:33:06 +0100

    Datensicherung

--- Rekursion/src/Fibonacci.java
+++ Rekursion/src/Fibonacci.java
@@ -1,13 +1,11 @@
-package Aufgabe_4_1;
-
-public class Aufgabe413 {
+public class Fibonacci {
     public static void main(String[] args) {
         int a=0;
         int b=1;
         int c=0;
-        int n=50;
+        int n=0;
         //System.out.println("Stufe 0: 0");
-        while(n<=10) {
+        while(n<=50) {
             c = a+b;
             System.out.println("Stufe "+n+":"+" "+a);
             a=b;
---
+++ Rekursion/src/TestDialog.java
@@ -0,0 +1,19 @@
+import javax.swing.JDialog;
+import javax.swing.WindowConstants;
+
+public class TestDialog extends JDialog {
+	private static final long serialVersionUID = 1L;
+
+	private TestPanel panel = new TestPanel();
+	
+	public TestDialog() {
+		getContentPane().add(panel);
+	}
+
+	public static void main(String[] arg) {
+		TestDialog dialog = new TestDialog();
+		dialog.setSize(1000,1000);
+		dialog.setVisible(true);
+		dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+	}
+}
